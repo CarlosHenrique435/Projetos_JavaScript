d3216beb16cd190a8d285350b8c864088b17ac5e
Carlos Henrique de Oliveira da Silva <carloshenriquedoliveiradasilva@gmail.com>
2023-07-19 19:39:36 -0300

    finalizado

--- exercicio 5 condicoes/exercicios/exercicio02/script.js
+++ exercicio 5 condicoes/exercicios/exercicio02/script.js
@@ -9,18 +9,49 @@ function verificar(){
     else{
         var calc = ano - nasc
         var fsex = document.getElementsByName('radsex')
+        var img = document.createElement("img")
+        img.setAttribute('id', 'foto')
         var genero = ''
-        var pic = document.getElementById("picture")
         if (fsex[0].checked){
             genero = 'Homem'
+            if (calc < 10){
+                img.setAttribute('src', 'midia/menino-crianca.png')
+                document.body.style.background = '#0192ce'
+            }
+            else if(calc >= 10 && calc <= 18){
+                img.setAttribute('src', 'midia/menino-jovem.png')
+                document.body.style.background = "#121324"
+            }
+            else if(calc > 18 && calc < 55){
+                img.setAttribute('src', 'midia/homem.png')
+                document.body.style.background = '#9a684b'
+            }
+            else{
+                img.setAttribute('src', 'midia/homem-velho.png')
+                document.body.style.background = '#a0adb7'
+            }
         }
         else{
             genero = 'Mulher'
-        }
-        result.innerHTML = `Detectamos ${genero} com ${calc} anos `
-        if (calc <= 14 && genero == "Homem"){
-            pic.src = 'midia/menina-jovem.png'
+            if(calc < 10){
+                img.setAttribute('src', 'midia/menina-crianca.png')
+                document.body.style.background = '#ae67a3'
+            }
+            else if(calc >= 10 && calc <= 18){
+                img.setAttribute('src', 'midia/menina-jovem.png')
+                document.body.style.background = '#9c9d9f'
+            }
+            else if(calc > 18 && calc < 55){
+                img.setAttribute('src', 'midia/mulher.png')
+                document.body.style.background = '#89a3a0'
+            }
+            else{
+                img.setAttribute('src', 'midia/mulher-velha.png')
+                document.body.style.background = '#014c5c'
+            }
         }
 
+        result.innerHTML = `Detectamos ${genero} com ${calc} anos `
+        result.appendChild(img)
     }
 }
