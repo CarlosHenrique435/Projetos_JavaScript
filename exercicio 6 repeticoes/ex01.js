9aa242adb9aa3a2aa6fdf03ab3b5ef5aecabd59f
Carlos Henrique de Oliveira da Silva <carloshenriquedoliveiradasilva@gmail.com>
2023-07-21 19:34:46 -0300

    aula completa

---
+++ exercicio 6 repeticoes/ex01.js
@@ -0,0 +1,12 @@
+var c = 1
+while (c <= 6){
+    console.log(`Linha número ${c}`)
+    c++
+}
+
+//Estrutura Do while
+
+do{
+    console.log(`Linha de número ${c}`)
+    c++
+}while(c <= 6)
---
+++ exercicio 6 repeticoes/ex02.js
@@ -0,0 +1,3 @@
+for (c=2; c<=10; c++){
+    console.log(`linha ${c}`);
+}
