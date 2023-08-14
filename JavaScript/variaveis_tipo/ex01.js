1945d9009edc5a13d44a79cfa76a88bfa217f521
Carlos Henrique de Oliveira da Silva <carloshenriquedoliveiradasilva@gmail.com>
2023-08-14 18:24:36 -0300

    completo

---
+++ JavaScript/variaveis_tipo/ex01.js
@@ -0,0 +1,14 @@
+"use strict"
+
+if (true){
+    let name = 'carlos'    
+}
+/* variavel name não pode ser chamada fora do if */
+
+
+const idade = 20
+console.log(idade)
+idade = 30
+console.log(idade)
+
+/* um variavel constante não pode ser mudada de valor*/
