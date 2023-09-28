8f39a7eb0409876c2f81babc29ec1019dbdf510b
Carlos Henrique de Oliveira da Silva <carloshenriquedoliveiradasilva@gmail.com>
2023-09-28 18:41:48 -0300

    aula this completa

--- JavaScript/funcoes/operador_this.js
+++ JavaScript/funcoes/operador_this.js
@@ -5,8 +5,8 @@ function Aluno(nome, nota){
     this.nota = nota
 
     this.dados_anonimos = function(){
-        setTimeout(function(){
-            this.nome = 'carlos'
+        setTimeout(()=>{//com arrow function não perdemos o this da instancia da função acima
+            this.nome = 'carlos'//nova instancia de this para o setTimeout
 
             console.log(this.nome)
             console.log(this.nota)
@@ -16,3 +16,6 @@ function Aluno(nome, nota){
 
 const al1= new Aluno('Carlos', 90)
 al1.dados_anonimos()
+
+//a função setTimeout cria uma nova instancia do this por isso
+//não conseguimos ter acesso ao this/referencia da função anterior
