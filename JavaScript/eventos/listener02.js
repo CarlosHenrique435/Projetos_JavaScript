7126ed95fdacc338c8e6b9765a634cae0acb519b
Carlos Henrique de Oliveira da Silva <carloshenriquedoliveiradasilva@gmail.com>
2023-10-09 20:34:51 -0300

    aula completa

---
+++ JavaScript/eventos/listener02.js
@@ -0,0 +1,22 @@
+const caixa1 = document.querySelector('#caixa1')
+const caixa2 = document.querySelector('#caixa2')
+const btn = document.querySelector('#btn_copiar')
+
+const todos = [...document.querySelectorAll('.curso')]
+
+todos.map((elemento)=>{
+    elemento.addEventListener('click', (evt)=>{
+        const curso = evt.target
+        curso.classList.toggle('selecionar')
+    })
+})
+
+btn.addEventListener('click', ()=>{
+    const selecionados =[... document.querySelectorAll('.selecionar')]
+    selecionados.map((elm)=>{
+        caixa2.appendChild(elm)
+    })
+})
+
+
+// * toggle -> se tiver o elemento ele remove/ se não tiver ele adiciona
