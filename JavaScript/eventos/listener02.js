c4430b46b2462ecf6ffee68a66d392877e6acd32
Carlos Henrique de Oliveira da Silva <carloshenriquedoliveiradasilva@gmail.com>
2023-10-18 20:20:19 -0300

    complementacao exercicio

--- JavaScript/eventos/listener02.js
+++ JavaScript/eventos/listener02.js
@@ -1,3 +1,40 @@
+// const caixa1 = document.querySelector('#caixa1')
+// const caixa2 = document.querySelector('#caixa2')
+// const btn = document.querySelector('#btn_copiar')
+
+// const todos = [...document.querySelectorAll('.curso')]
+
+// todos.map((elemento)=>{
+//     elemento.addEventListener('click', (evt)=>{
+//         const curso = evt.target
+//         curso.classList.toggle('selecionar')
+//         console.log(curso)
+//     })
+// })
+
+// btn.addEventListener('click', ()=>{
+//     const selecionados =[... document.querySelectorAll('.selecionar')]
+
+//     selecionados.map((elm)=>{
+//         if (elm.parentNode == caixa1){
+//             caixa2.appendChild(elm)
+//             console.log(elm)
+//         }
+//         else{
+//             caixa1.appendChild(elm)
+//         }
+//         console.log(elm.parentNode)
+        
+//     })
+// })
+
+
+// * toggle -> se tiver o elemento ele remove/ se não tiver ele adiciona
+
+// SEGUNDA OPÇÃO, POREM AGORA TODOS SELECIONADOS PERTENCEM AO
+//CAIXA02 E OS NÃO SELECIONADOS AO CAIXA01
+
+
 const caixa1 = document.querySelector('#caixa1')
 const caixa2 = document.querySelector('#caixa2')
 const btn = document.querySelector('#btn_copiar')
@@ -15,18 +52,18 @@ todos.map((elemento)=>{
 btn.addEventListener('click', ()=>{
     const selecionados =[... document.querySelectorAll('.selecionar')]
 
+    const notSelecionados = [...document.querySelectorAll('.curso:not(.selecionar)')]
+
+    console.log(notSelecionados)
+
     selecionados.map((elm)=>{
-        if (elm.parentNode == caixa1){
-            caixa2.appendChild(elm)
-            console.log(elm)
-        }
-        else{
-            caixa1.appendChild(elm)
-        }
-        console.log(elm.parentNode)
-        
+        caixa2.appendChild(elm)
+    })
+    notSelecionados.map((elm)=>{
+        caixa1.appendChild(elm)
     })
 })
 
+//A variavel notSelecionados contem um querrySelectorAll
+// usamos o :not(classe ou id) --> Caso seja falso pegue
 
-// * toggle -> se tiver o elemento ele remove/ se não tiver ele adiciona
