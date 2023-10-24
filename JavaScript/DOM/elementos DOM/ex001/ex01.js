9d7b71d3a6ad361ab4ebb0c95f67af5867b0d693
Carlos Henrique de Oliveira da Silva <carloshenriquedoliveiradasilva@gmail.com>
2023-10-24 20:30:00 -0300

    aula completa

---
+++ JavaScript/DOM/elementos DOM/ex001/ex01.js
@@ -0,0 +1,6 @@
+const caixa1 =  document.querySelector('#caixa1')
+const btn_c = [...document.querySelectorAll('.curso')]
+
+console.log(caixa1.children[0])
+console.log(caixa1.lastElementChild)
+console.log(document.getRootNode())
