13463ecc84c7ca36eae96eeb271d25ff1ce1a7c0
Carlos Henrique de Oliveira da Silva <carloshenriquedoliveiradasilva@gmail.com>
2023-10-24 20:50:57 -0300

    aula completa

--- JavaScript/DOM/elementos DOM/ex001/ex01.js
+++ JavaScript/DOM/elementos DOM/ex001/ex01.js
@@ -1,6 +1,17 @@
 const caixa1 =  document.querySelector('#caixa1')
 const btn_c = [...document.querySelectorAll('.curso')]
+const c1_2 = document.querySelector('#c1_2')
 
-console.log(caixa1.children[0])
-console.log(caixa1.lastElementChild)
-console.log(document.getRootNode())
+console.log(c1_2.parentElement.parentElement.parentElement.children[4])
+
+
+console.log(caixa1.hasChildNodes())
+
+console.log(caixa1.children.length > 1 ? 'Possui filhos' : 'Não possui filhos')
+
+
+console.log(caixa1.children[1].innerHTML = 'Teste')
+
+// console.log(caixa1.children[0])
+// console.log(caixa1.lastElementChild)
+// console.log(document.getRootNode())
